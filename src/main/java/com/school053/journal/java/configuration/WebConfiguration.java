411bb9c83de529fab12015a8df9a0151841b0a76
Igor Miniailo <ihor.minyaylo@gmail.com>
2017-08-18 13:27:05 +0300

    Add plugin for pom.xml(without web.xml)

--- src/main/java/com/school053/journal/java/configuration/WebConfiguration.java
+++ src/main/java/com/school053/journal/java/configuration/WebConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -12,6 +13,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @ComponentScan(basePackages = "com.school053.journal")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/**")
+                .addResourceLocations("/WEB-INF/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
