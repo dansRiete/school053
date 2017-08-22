b7d5b4b3001740562b639daf879f29ad99755341
Alexandr Kruglov <alkruglov777@gmail.com>
2017-08-22 14:10:45 +0300

    Created interface LessonEventService.

---
+++ src/main/java/com/school053/journal/java/service/LessonEventService.java
@@ -0,0 +1,11 @@
+package com.school053.journal.java.service;
+
+import java.util.List;
+
+import com.school053.journal.java.model.events.LessonEvent;
+
+public interface LessonEventService {
+	
+	public List<LessonEvent> getLessonEvents();
+	
+}
