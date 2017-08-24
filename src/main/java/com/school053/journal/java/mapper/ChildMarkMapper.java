eec02691c6b340fea12b99f40667c8971fd06439
Alexandr Kruglov <alkruglov777@gmail.com>
2017-08-24 12:44:46 +0300

    Created ChildMarkMapper.

---
+++ src/main/java/com/school053/journal/java/mapper/ChildMarkMapper.java
@@ -0,0 +1,20 @@
+package com.school053.journal.java.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.school053.journal.java.dto.ChildMarkDto;
+import com.school053.journal.java.model.events.ChildMark;
+
+@Mapper
+public interface ChildMarkMapper {
+	
+	@Mappings({
+		@Mapping(target = "id", source = "childMark.id"),
+		@Mapping(target = "mark", source = "childMark.mark"),
+		@Mapping(target = "childFirstName", source = "childMark.child.firsName"),
+		@Mapping (target = "lessonEventTypeName", source = "childMark.lessonEventType.name")
+	})
+	ChildMarkDto toDto(ChildMark childMark);
+}
