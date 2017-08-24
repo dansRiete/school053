36a123cd5eec89c75a5eee23693b58727e20245a
Alexandr Kruglov <alkruglov777@gmail.com>
2017-08-24 13:31:21 +0300

    Created class ChildMarkServiceImpl.

---
+++ src/main/java/com/school053/journal/java/service/impl/ChildMarkServiceImpl.java
@@ -0,0 +1,26 @@
+package com.school053.journal.java.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.school053.journal.java.dao.ChildMarkDao;
+import com.school053.journal.java.dto.ChildMarkDto;
+import com.school053.journal.java.mapper.ChildMarkMapper;
+import com.school053.journal.java.service.ChildMarkService;
+
+@Service
+public class ChildMarkServiceImpl implements ChildMarkService {
+
+	@Autowired
+	private ChildMarkDao childMarkDao;
+
+	@Override
+	public List<ChildMarkDto> getBy(String childId, String subjectId) {
+		return childMarkDao.findBy(childId, subjectId).stream().map(Mappers.getMapper(ChildMarkMapper.class)::toDto)
+				.collect(Collectors.toList());
+	}
+}
