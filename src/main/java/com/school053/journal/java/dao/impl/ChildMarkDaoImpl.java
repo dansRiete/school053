a33d53f9a893c3a53ce2c32ab973762144740615
Alexandr Kruglov <alkruglov777@gmail.com>
2017-08-24 12:29:40 +0300

    Created class ChildMarkDto. Added to class ChildMarkDaoImpl @Repository.

--- src/main/java/com/school053/journal/java/dao/impl/ChildMarkDaoImpl.java
+++ src/main/java/com/school053/journal/java/dao/impl/ChildMarkDaoImpl.java
@@ -2,10 +2,13 @@ package com.school053.journal.java.dao.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.school053.journal.java.dao.AbstractDao;
 import com.school053.journal.java.dao.ChildMarkDao;
 import com.school053.journal.java.model.events.ChildMark;
 
+@Repository(value = "ChildMarkDao")
 public class ChildMarkDaoImpl extends AbstractDao<ChildMark> implements ChildMarkDao {
 
 	@Override
--- src/main/java/com/school053/journal/java/dto/ChildMarkDto.java
+++ src/main/java/com/school053/journal/java/dto/ChildMarkDto.java
@@ -1,5 +1,50 @@
 package com.school053.journal.java.dto;
 
 public class ChildMarkDto {
+	private String id;
+	private String mark;
+	private String childFirstName;
+	private String lessonEventTypeName;
+	
+	public ChildMarkDto() {
+	}
 
+	public ChildMarkDto(String id, String mark, String childFirstName, String lessonEventTypeName) {
+		this.id = id;
+		this.mark = mark;
+		this.childFirstName = childFirstName;
+		this.lessonEventTypeName = lessonEventTypeName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getChildFirstName() {
+		return childFirstName;
+	}
+
+	public void setChildFirstName(String childFirstName) {
+		this.childFirstName = childFirstName;
+	}
+
+	public String getLessonEventTypeName() {
+		return lessonEventTypeName;
+	}
+
+	public void setLessonEventTypeName(String lessonEventTypeName) {
+		this.lessonEventTypeName = lessonEventTypeName;
+	}
 }
